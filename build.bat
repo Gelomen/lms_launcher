6e4f3985ea336a8ba488389b6532addd40ddc9fd
Gelomen <gelomenchen@gmail.com>
2026-08-28 23:28:36 +0800

    clean

--- build.bat
+++ build.bat
@@ -1,5 +1,3 @@
 @echo off
-chcp 65001 >nul
-setlocal enabledelayedexpansion
 
-npx tsc -p tsconfig.main.json && npm run build && npx electron-builder --win portable && pause
+npx tsc -p tsconfig.main.json && npm run build && npx electron-builder --win portable
